afe371e26341247ad8811cbf7f347526dbeb4638
ddevito <ddevito@tuenti.com>
2019-08-19 18:46:10 -0300

    Session 5 - Video 32

--- app/src/main/java/course/intermediate/notes/views/TodoView.kt
+++ app/src/main/java/course/intermediate/notes/views/TodoView.kt
@@ -1,7 +1,6 @@
 package course.intermediate.notes.views
 
 import android.content.Context
-import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import course.intermediate.notes.models.Todo
@@ -18,7 +17,7 @@ class TodoView @JvmOverloads constructor(
         descriptionView.text = todo.description
 
         if (todo.isComplete) {
-            createStrikeThrough()
+            descriptionView.setStrikeThrough()
         }
 
         setUpCheckStateListener(todo, callback)
@@ -29,22 +28,10 @@ class TodoView @JvmOverloads constructor(
             todo.isComplete = isChecked
             callback?.invoke(isChecked)
             if (isChecked) {
-                createStrikeThrough()
+                descriptionView.setStrikeThrough()
             } else {
-                removeStrikeThrough()
+                descriptionView.removeStrikeThrough()
             }
-        }
-    }
-
-    private fun createStrikeThrough() {
-        descriptionView.apply {
-            paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-        }
-    }
-
-    private fun removeStrikeThrough() {
-        descriptionView.apply {
-            paintFlags = paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
         }
     }
 }
