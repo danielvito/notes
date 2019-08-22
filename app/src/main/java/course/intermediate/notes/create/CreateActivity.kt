c52614ba23ecdf6b68bc3b65d60d665611ac49ec
ddevito <ddevito@tuenti.com>
2019-08-22 18:18:26 -0300

    Session 7 - Video 39

--- app/src/main/java/course/intermediate/notes/create/CreateActivity.kt
+++ app/src/main/java/course/intermediate/notes/create/CreateActivity.kt
@@ -1,25 +1,49 @@
 package course.intermediate.notes.create
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import course.intermediate.notes.navigation.NavigationActivity
 import intermediate.course.notes.R
-import kotlinx.android.synthetic.main.activity_create.*
 
-class CreateActivity : AppCompatActivity() {
+class CreateActivity : AppCompatActivity(), CreateTaskFragment.OnFragmentInteractionListener, CreateNoteFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
+        supportActionBar?.title = ""
+
         intent?.getStringExtra(NavigationActivity.FRAGMENT_TYPE_KEY).run {
-            textView.text = if (this == NavigationActivity.FRAGMENT_VALUE_TASK) {
-                "this is a task"
+
+            if (this == NavigationActivity.FRAGMENT_VALUE_TASK) {
+                createFragment(CreateTaskFragment.newInstance())
             } else if (this == NavigationActivity.FRAGMENT_VALUE_NOTE) {
-                "this is a note"
-            } else {
-                "something went wrong"
+                createFragment(CreateNoteFragment.newInstance())
             }
+
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.saveItem -> Toast.makeText(this, "SaveCliked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragmentHolder, fragment).commit()
+    }
+
+    override fun onFragmentInteraction() {
     }
 }
