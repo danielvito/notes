dbdc42d7666236b5e633c84120ed8e9e6abb6839
ddevito <ddevito@tuenti.com>
2019-08-22 16:57:40 -0300

    Session 6 - Video 37

--- app/src/main/java/course/intermediate/notes/notes/NoteViewModel.kt
+++ app/src/main/java/course/intermediate/notes/notes/NoteViewModel.kt
@@ -3,9 +3,9 @@ package course.intermediate.notes.notes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import course.intermediate.notes.foundations.ApplicationScope
 import course.intermediate.notes.models.Note
 import toothpick.Toothpick
-import toothpick.config.Module
 import javax.inject.Inject
 
 class NoteViewModel : ViewModel(), NoteListViewContract {
@@ -17,11 +17,7 @@ class NoteViewModel : ViewModel(), NoteListViewContract {
     val noteListLiveData: LiveData<List<Note>> = _noteListLiveData
 
     init {
-        val scope = Toothpick.openScope(this)
-        scope.installModules(Module().apply {
-            bind(INoteModel::class.java).toInstance(NoteLocalModel())
-        })
-        Toothpick.inject(this, scope)
+        Toothpick.inject(this, ApplicationScope.scope)
         _noteListLiveData.postValue(model.getFakeData())
     }
 
--- app/src/main/java/course/intermediate/notes/tasks/TaskViewModel.kt
+++ app/src/main/java/course/intermediate/notes/tasks/TaskViewModel.kt
@@ -3,9 +3,9 @@ package course.intermediate.notes.tasks
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import course.intermediate.notes.foundations.ApplicationScope
 import course.intermediate.notes.models.Task
 import toothpick.Toothpick
-import toothpick.config.Module
 import javax.inject.Inject
 
 class TaskViewModel : ViewModel(), TaskListViewContract {
@@ -17,11 +17,7 @@ class TaskViewModel : ViewModel(), TaskListViewContract {
     val taskListLiveData: LiveData<List<Task>> = _taskListLiveData
 
     init {
-        val scope = Toothpick.openScope(this)
-        scope.installModules(Module().apply {
-            bind(ITaskModel::class.java).toInstance(TaskLocalModel())
-        })
-        Toothpick.inject(this, scope)
+        Toothpick.inject(this, ApplicationScope.scope)
         _taskListLiveData.postValue(model.getFakeData())
     }
 
