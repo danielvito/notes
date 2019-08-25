45247585f83d248bded0f1889ee217bfaad1b6b6
ddevito <ddevito@tuenti.com>
2019-08-25 10:38:43 -0300

    Session 7 - Video 41

--- app/src/main/java/course/intermediate/notes/create/CreateTaskFragment.kt
+++ app/src/main/java/course/intermediate/notes/create/CreateTaskFragment.kt
@@ -2,11 +2,17 @@ package course.intermediate.notes.create
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import course.intermediate.notes.foundations.StateChangeTextWatcher
+import course.intermediate.notes.views.CreateTodoView
 import intermediate.course.notes.R
+import kotlinx.android.synthetic.main.fragment_create_task.*
+import kotlinx.android.synthetic.main.view_create_task.view.*
+import kotlinx.android.synthetic.main.view_create_todo.view.*
 
 class CreateTaskFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
@@ -17,6 +23,40 @@ class CreateTaskFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_task, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createTaskView.taskEditText.addTextChangedListener(object: StateChangeTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+                if(!s.isNullOrEmpty() && previusValue.isNullOrEmpty()) {
+                    addTodoView()
+                }
+
+                super.afterTextChanged(s)
+            }
+        })
+    }
+
+    private fun addTodoView() {
+        val view = LayoutInflater.from(context).inflate(R.layout.view_create_todo, containerView,false) as CreateTodoView
+        view.todoEditText.addTextChangedListener(object: StateChangeTextWatcher() {
+            override fun afterTextChanged(s: Editable?) {
+
+                if(!s.isNullOrEmpty() && previusValue.isNullOrEmpty()) {
+                    addTodoView()
+                } else if (!previusValue.isNullOrEmpty() && s.isNullOrEmpty()) {
+                    removeTodoView(view)
+                }
+                super.afterTextChanged(s)
+            }
+        })
+        containerView.addView(view)
+    }
+
+    private fun removeTodoView(view: View) {
+        containerView.removeView(view)
     }
 
     override fun onAttach(context: Context) {
---
+++ app/src/main/java/course/intermediate/notes/foundations/StateChangeTextWatcher.kt
@@ -0,0 +1,21 @@
+package course.intermediate.notes.foundations
+
+import android.text.Editable
+import android.text.TextWatcher
+
+open class StateChangeTextWatcher: TextWatcher {
+
+    var previusValue: String? = null
+
+    override fun afterTextChanged(s: Editable?) {
+        previusValue = s?.toString()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // no op
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // no op
+    }
+}
