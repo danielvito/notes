2c5d6392fe334983eb611d6735e65f0b661fcafd
ddevito <ddevito@tuenti.com>
2019-08-26 19:00:20 -0300

    Session 8 - Video 47

---
+++ app/src/main/java/course/intermediate/notes/application/NoteApplication.kt
@@ -0,0 +1,14 @@
+package course.intermediate.notes.application
+
+import android.app.Application
+
+class NoteApplication : Application() {
+
+    companion object {
+        lateinit var instance: Application
+    }
+
+    init {
+        instance = this
+    }
+}
---
+++ app/src/main/java/course/intermediate/notes/database/RoomDatabaseClient.kt
@@ -0,0 +1,27 @@
+package course.intermediate.notes.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+const val DATABASE_SCHEMA_VERSION = 1
+const val DB_NAME = "local-db"
+
+@Database(version = DATABASE_SCHEMA_VERSION, entities = [])
+abstract class RoomDatabaseClient : RoomDatabase() {
+
+    companion object {
+        private var instance: RoomDatabaseClient? = null
+        fun getInstance(context: Context): RoomDatabaseClient {
+            if (instance == null) {
+                instance = createDatabase(context)
+            }
+            return instance!!
+        }
+
+        private fun createDatabase(context: Context): RoomDatabaseClient {
+            return Room.databaseBuilder(context, RoomDatabaseClient::class.java, DB_NAME).build()
+        }
+    }
+}
